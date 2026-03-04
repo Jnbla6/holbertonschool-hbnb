5a45e9d40977e980ef1e4643ffc32edf05e0231f
m7md-d <wazrahp@gmail.com>
2026-03-05 01:09:07 +0300

    feat: add authentication namespace to API

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,6 +4,7 @@ from app.api.v1.amenities import api as amenity_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as place_ns
+from app.api.v1.auth import api as auth_ns
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 
@@ -18,6 +19,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(place_ns, path='/api/v1/places')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     jwt.init_app(app)
     bcrypt.init_app(app)
